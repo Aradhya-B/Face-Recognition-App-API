ed36f2e23560f534d5a588504c97d857b08fe802
Aradhya Bansal <aradhyabansal2000@gmail.com>
2019-09-09 23:59:47 -0400

    syntax errors

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('App is running on port ${process.env.port}');
+  console.log(`App is running on port ${process.env.PORT}`);
 });
